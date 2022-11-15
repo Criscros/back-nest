f3040a08754c146b5a5cd33b095efc11e9bad38d
david <christian.murillo@fletx.co>
2022-11-14 19:03:38 -0500

    start

--- dist/health/controller/health/health.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { HealthCheckService } from '@nestjs/terminus';
-declare class HealthController {
-    private health;
-    constructor(health: HealthCheckService);
-    check(): Promise<import("@nestjs/terminus").HealthCheckResult>;
-}
-export default HealthController;
